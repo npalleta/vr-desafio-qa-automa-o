5d841a2202e2bc9a73665928a5f78f47e77ad8a8
Niky Lima <niky.palleta@ascential.com>
2023-05-07 00:31:41 -0300

    YAML.load_file has been changed

--- httparty_src/tests/features/support/settings.rb
+++ httparty_src/tests/features/support/settings.rb
@@ -3,7 +3,7 @@ require 'yaml'
 class Settings
   def self.get_environment_path
     yaml_file_path = "/config/#{ENVIRONMENT}.yaml"
-    YAML.load_file(File.dirname(__FILE__) + yaml_file_path)
+    YAML.load_file("#{File.dirname(__FILE__)}#{yaml_file_path}")
   end
 
   def self.get_homol_url(path)
